7793e01cf2db68eef97258f980fe9d9bae1a7f24
ho3infj <farajihossein1379@gmail.com>
2023-07-13 14:53:59 +0330

    Convert titles to links

--- home/views.py
+++ home/views.py
@@ -9,3 +9,7 @@ def hello(request):
 def home(request):
     all = Todo.objects.all()
     return render(request , 'home.html' , {'todos' : all})
+
+def detail(request , todo_id):
+    todo = Todo.objects.get(id = todo_id)
+    return render(request , 'detail.html' , {'todo' : todo})
