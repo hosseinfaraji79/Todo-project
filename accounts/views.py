16c1d4faa410aefa7cf043f6e2146f49f7e17f3e
ho3infj <farajihossein1379@gmail.com>
2023-07-21 13:46:45 +0330

    How to create by the user

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render , redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def user_register(request):
-    return HttpResponse('User registration page')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(cd['username'] , cd['email'] , cd['password'])
+            user.first_name = cd['first_name']
+            user.last_name = cd['last_name']
+            user.save()
+            messages.success(request , 'ریجستر به درستی انجام شد', 'success')
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request , 'register.html' , {'form':form})
